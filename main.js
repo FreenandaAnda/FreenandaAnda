b1fa33989576db28922890b5dd5c3c764ca989c8
FreenandaAnda <160641638+FreenandaAnda@users.noreply.github.com>
2024-02-24 19:37:00 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function muestra_oculta (id) {
+
+    let div = caja7.getElementById (id);
+    if (div.style.display =="none"){
+        
+    }
+    else{
+        div.style.display = "flex";
+}
+
+}
